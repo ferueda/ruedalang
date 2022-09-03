6b86b6ebb928696b1e554914b888b180c494e372
ferueda <fe.rueda@outlook.com>
2022-09-03 13:31:43 -0700

    create token test suite

---
+++ token/token_test.go
@@ -0,0 +1,47 @@
+package token
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestNewToken(t *testing.T) {
+	testTable := []struct {
+		input           rune
+		expectedType    TokenType
+		expectedLiteral string
+	}{
+		{'=', TokenType("="), "="},
+		{'+', TokenType("+"), "+"},
+		{'{', TokenType("{"), "{"},
+		{'}', TokenType("}"), "}"},
+		{'(', TokenType("("), "("},
+		{')', TokenType(")"), ")"},
+		{';', TokenType(";"), ";"},
+		{',', TokenType(","), ","},
+	}
+
+	for _, tt := range testTable {
+		tok := NewToken(TokenType(tt.input), byte(tt.input))
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("invalid token. Want %q, got %q", tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("invalid literal. Want %q, got %q", tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
+func TestLookupIdentifier(t *testing.T) {
+	for key, val := range keywords {
+		if LookupIdentifier(string(key)) != val {
+			t.Errorf("Lookup of %s failed", key)
+		}
+
+		if LookupIdentifier(strings.ToUpper(string(key))) != IDENT {
+			t.Errorf("Lookup of %s failed", key)
+		}
+	}
+}
