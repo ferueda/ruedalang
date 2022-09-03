2aa04a7ccc4f8df722f540382f05da3f47a0ead9
ferueda <fe.rueda@outlook.com>
2022-09-03 13:31:20 -0700

    create barebones lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,104 @@
+package lexer
+
+import (
+	"github.com/ferueda/rueda/token"
+)
+
+// Lexer represents the lexer in the lexical analysis process. It has the required methods to perform lexical analysis steps.
+type Lexer struct {
+	input   string
+	pos     int  // current position
+	readPos int  // current reading position (next position after pos)
+	ch      byte // current char
+}
+
+// NewLexer returns a new Lexer struct value.
+// It also calls Lexer's readChar method once when initializing.
+func NewLexer(input string) *Lexer {
+	l := Lexer{input: input}
+	l.readChar()
+	return &l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPos >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPos]
+	}
+	l.pos = l.readPos
+	l.readPos += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	l.skipWhitespace()
+	switch l.ch {
+	case '=':
+		tok = token.NewToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = token.NewToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = token.NewToken(token.LPAREN, l.ch)
+	case ')':
+		tok = token.NewToken(token.RPAREN, l.ch)
+	case ',':
+		tok = token.NewToken(token.COMMA, l.ch)
+	case '+':
+		tok = token.NewToken(token.PLUS, l.ch)
+	case '{':
+		tok = token.NewToken(token.LBRACE, l.ch)
+	case '}':
+		tok = token.NewToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdentifier(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = token.NewToken(token.ILLEGAL, l.ch)
+		}
+	}
+
+	l.readChar()
+	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	pos := l.pos
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[pos:l.pos]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) readNumber() string {
+	pos := l.pos
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[pos:l.pos]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+// skipWhitespace skips over whitespaces by calling Lexer's readChar method.
+// Whitespaces include like space, newline, tab, and carriage return.
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
