d478ba1afdeb6ef9911a394004b0bf0c1ea262e1
ferueda <fe.rueda@outlook.com>
2022-09-03 17:24:08 -0700

    add support for missing tokens

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,18 +1,22 @@
+// Package lexer implements a lexer for Rueda source text.
+// It takes an input string as source and then tokenizes it
+// through repeated calls to the NextToken method.
 package lexer
 
 import (
 	"github.com/ferueda/rueda/token"
 )
 
-// Lexer represents the lexer in the lexical analysis process. It has the required methods to perform lexical analysis steps.
+// Lexer contains the lexer's internal state while processing a given
+// source text.
 type Lexer struct {
 	input   string
 	pos     int  // current position
 	readPos int  // current reading position (next position after pos)
-	ch      byte // current char
+	ch      byte // current character
 }
 
-// NewLexer returns a new Lexer struct value.
+// NewLexer returns a new Lexer struct value from a string input.
 // It also calls Lexer's readChar method once when initializing.
 func NewLexer(input string) *Lexer {
 	l := Lexer{input: input}
@@ -20,6 +24,7 @@ func NewLexer(input string) *Lexer {
 	return &l
 }
 
+// readChar reads one character at a time from the Lexer's input
 func (l *Lexer) readChar() {
 	if l.readPos >= len(l.input) {
 		l.ch = 0
@@ -30,22 +35,47 @@ func (l *Lexer) readChar() {
 	l.readPos += 1
 }
 
+// NextToken reads the next token from the Lexer's input, skipping white spaces.
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 	l.skipWhitespace()
 	switch l.ch {
 	case '=':
-		tok = token.NewToken(token.ASSIGN, l.ch)
+		if l.peek() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = token.NewToken(token.ASSIGN, l.ch)
+		}
+	case '+':
+		tok = token.NewToken(token.PLUS, l.ch)
+	case '-':
+		tok = token.NewToken(token.MINUS, l.ch)
+	case '!':
+		if l.peek() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = token.NewToken(token.BANG, l.ch)
+		}
+	case '/':
+		tok = token.NewToken(token.SLASH, l.ch)
+	case '*':
+		tok = token.NewToken(token.ASTERISK, l.ch)
+	case '<':
+		tok = token.NewToken(token.LT, l.ch)
+	case '>':
+		tok = token.NewToken(token.GT, l.ch)
 	case ';':
 		tok = token.NewToken(token.SEMICOLON, l.ch)
+	case ',':
+		tok = token.NewToken(token.COMMA, l.ch)
 	case '(':
 		tok = token.NewToken(token.LPAREN, l.ch)
 	case ')':
 		tok = token.NewToken(token.RPAREN, l.ch)
-	case ',':
-		tok = token.NewToken(token.COMMA, l.ch)
-	case '+':
-		tok = token.NewToken(token.PLUS, l.ch)
 	case '{':
 		tok = token.NewToken(token.LBRACE, l.ch)
 	case '}':
@@ -56,7 +86,7 @@ func (l *Lexer) NextToken() token.Token {
 	default:
 		if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
-			tok.Type = token.LookupIdentifier(tok.Literal)
+			tok.Type = token.Lookup(tok.Literal)
 			return tok
 		} else if isDigit(l.ch) {
 			tok.Literal = l.readNumber()
@@ -96,9 +126,17 @@ func isDigit(ch byte) bool {
 }
 
 // skipWhitespace skips over whitespaces by calling Lexer's readChar method.
-// Whitespaces include like space, newline, tab, and carriage return.
+// Whitespaces include space, newline, tab, and carriage return.
 func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
+	}
+}
+
+func (l *Lexer) peek() byte {
+	if l.readPos >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPos]
 	}
 }
