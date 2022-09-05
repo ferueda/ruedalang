c114af58904f71254595d07261c1e8e01db3abc2
ferueda <fe.rueda@outlook.com>
2022-09-04 23:35:50 -0700

    add parser test suite

---
+++ parser/parser_test.go
@@ -0,0 +1,79 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/ferueda/ruedalang/ast"
+	"github.com/ferueda/ruedalang/lexer"
+)
+
+func TestVarStatements(t *testing.T) {
+	input := `
+		var x = 5;
+		var y = 10;
+
+		var foobar = 838383;
+	`
+
+	l := lexer.NewLexer(input)
+	p := NewParser(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. Got=%d", len(program.Statements))
+	}
+
+	testTable := []struct {
+		expecttedIdent string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range testTable {
+		stmt := program.Statements[i]
+		if !testVarStatement(t, stmt, tt.expecttedIdent) {
+			return
+		}
+	}
+}
+
+func testVarStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "var" {
+		t.Errorf("s.TokenLiteral not 'var'. got=%q", s.TokenLiteral())
+		return false
+	}
+	varStmt, ok := s.(*ast.VarStatement)
+	if !ok {
+		t.Errorf("s not *ast.VarStatement. got=%T", s)
+		return false
+	}
+	if varStmt.Name.Value != name {
+		t.Errorf("varStmt.Name.Value not '%s'. got=%s", name, varStmt.Name.Value)
+		return false
+	}
+	if varStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not '%s'. got=%s", name, varStmt.Name)
+		return false
+	}
+	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
