ebf21bdf9102c5dc667559c2a9243c0aebaeb791
ferueda <fe.rueda@outlook.com>
2022-09-05 14:43:05 -0700

    Add ReturnStatement parser test suite

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -77,3 +77,37 @@ func checkParserErrors(t *testing.T, p *Parser) {
 	}
 	t.FailNow()
 }
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+		return 5;
+		return 10;
+
+		return 993322;
+	`
+
+	l := lexer.NewLexer(input)
+	p := NewParser(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral() not 'return, got=%q", returnStmt.TokenLiteral())
+		}
+	}
+}
