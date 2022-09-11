6cce549d46c23443c3a4257f4dbfa3ba29aded9b
ferueda <fe.rueda@outlook.com>
2022-09-10 21:02:38 -0700

    add parseBlockStatement

--- parser/parser.go
+++ parser/parser.go
@@ -255,6 +255,19 @@ func (p *Parser) parseGroupedExpression() ast.Expression {
 	return exp
 }
 
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.curToken}
+	block.Statements = []ast.Statement{}
+	p.nextToken()
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return block
+}
 // peekPrecedence method returns the precedence associated with the
 // token type of p.peekToken.
 func (p *Parser) peekPrecedence() int {
