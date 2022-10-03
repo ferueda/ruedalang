e809b1e889e6edd210373700b7779c4b7969fdaf
ferueda <fe.rueda@outlook.com>
2022-10-02 18:11:08 -0700

    add evaluator package to repl

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/ferueda/ruedalang/evaluator"
 	"github.com/ferueda/ruedalang/lexer"
 	"github.com/ferueda/ruedalang/parser"
 )
@@ -31,8 +32,12 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
